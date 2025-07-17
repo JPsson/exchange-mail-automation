006596e04faad9be315dee681277434327316442
JPsson <jakob.pettersson01@gmail.com>
2025-07-17 13:40:32 +0200

    Clean UI fixes

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    center: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
